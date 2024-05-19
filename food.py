cc279b75441e8684b1dff758fa76c3e36b78d58d
Nurgazy <amanbekovnurgazy@mail.ru>
2024-05-19 13:36:09 +0600

    first commit

--- food.py
+++ food.py
@@ -3,16 +3,17 @@ import random
 
 
 class Food(Turtle):
-
+    # initialize food instance inherits from Turtle class
     def __init__(self):
         super().__init__()
-        self.shape("turtle")
+        self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("green")
         self.speed("fastest")
         self.refresh()
 
+    # Creats a new position for food instance
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
--- scoreboard.py
+++ scoreboard.py
@@ -25,3 +25,4 @@ class Scoreboard(Turtle):
         self.clear()
         self.score += 1
         self.update_scoreboard()
+
